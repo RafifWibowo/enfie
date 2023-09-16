70fb1042cba8a5e23bd65ee82f3dd4e24c26138e
RafifWibowo <rafifnaufaldi2@gmail.com>
2023-09-16 10:25:05 +0700

    edit index

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.use("/users", users);
 app.use("/datas", datas);
 app.use("/patients", patients);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
